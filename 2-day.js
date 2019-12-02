201965fce7e9b4db9693ff3c5dce6ddcb02abebf
Ante Barić <ante.baric@burza.hr>
2019-12-02 10:25:36 +0100

    2-day part 1 completed

---
+++ 2-day.js
@@ -0,0 +1,36 @@
+require('es6-promise').polyfill()
+require('isomorphic-fetch')
+
+const main = async() => {
+    const data = await fetch('https://pastebin.com/raw/Amih4G6D').then(response => response.text())
+    const input = data.split(',').map(code => +code)
+    input[1] = 12
+    input[2] = 2
+
+    const programSize = input.length
+    let index = 0
+    let code = input[index]
+
+    while (code !== 99 && index < programSize) {
+        const a = input[input[index + 1]]
+        const b = input[input[index + 2]]
+
+        switch (code) {
+            case 1:
+                input[input[index + 3]] = a + b
+                break
+            case 2:
+                input[input[index + 3]] = a * b
+                break
+            default:
+                break
+        }
+
+        index += 4
+        code = input[index]
+    }
+
+    console.log(input[0])
+}
+
+main()
